3fa19479154596a6bb5787be01e3e914b701adbc
Ken Tran <kentran08@gmail.com>
2014-08-26 12:37:22 -0700

    added a test class named OKFactory

---
+++ NLCoach/OKFactory.h
@@ -0,0 +1,13 @@
+//
+//  OKFactory.h
+//  NLCoach
+//
+//  Created by Ken Tran on 2014-08-26.
+//  Copyright (c) 2014 Ken Tran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OKFactory : NSObject
+
+@end
---
+++ NLCoach/OKFactory.m
@@ -0,0 +1,13 @@
+//
+//  OKFactory.m
+//  NLCoach
+//
+//  Created by Ken Tran on 2014-08-26.
+//  Copyright (c) 2014 Ken Tran. All rights reserved.
+//
+
+#import "OKFactory.h"
+
+@implementation OKFactory
+
+@end
